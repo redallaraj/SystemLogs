c0ef4bf1792ef0c8291836f986cd8567d57da61b
redallaraj <redallaraj@gmail.com>
2019-12-04 14:59:12 +0100

    First Commit

---
+++ LOG/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txt_ip = New System.Windows.Forms.TextBox()
+        Me.txt_mac = New System.Windows.Forms.TextBox()
+        Me.txt_pc = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tm = New System.Windows.Forms.Timer(Me.components)
+        Me.tm2 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gray
+        Me.Panel1.Controls.Add(Me.txt_ip)
+        Me.Panel1.Controls.Add(Me.txt_mac)
+        Me.Panel1.Controls.Add(Me.txt_pc)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(20, 225)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(386, 168)
+        Me.Panel1.TabIndex = 19
+        '
+        'txt_ip
+        '
+        Me.txt_ip.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_ip.Location = New System.Drawing.Point(115, 108)
+        Me.txt_ip.Name = "txt_ip"
+        Me.txt_ip.ReadOnly = True
+        Me.txt_ip.Size = New System.Drawing.Size(207, 20)
+        Me.txt_ip.TabIndex = 14
+        '
+        'txt_mac
+        '
+        Me.txt_mac.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_mac.Location = New System.Drawing.Point(115, 72)
+        Me.txt_mac.Name = "txt_mac"
+        Me.txt_mac.ReadOnly = True
+        Me.txt_mac.Size = New System.Drawing.Size(207, 20)
+        Me.txt_mac.TabIndex = 13
+        '
+        'txt_pc
+        '
+        Me.txt_pc.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_pc.Location = New System.Drawing.Point(115, 32)
+        Me.txt_pc.Name = "txt_pc"
+        Me.txt_pc.ReadOnly = True
+        Me.txt_pc.Size = New System.Drawing.Size(207, 20)
+        Me.txt_pc.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 111)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(27, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "IP :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(11, 75)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(104, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "MAC ADDRESS :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(11, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "PC NAME :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 169)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Label2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Label1"
+        '
+        'tm
+        '
+        Me.tm.Interval = 10
+        '
+        'tm2
+        '
+        Me.tm2.Interval = 300000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipText = "Click me !"
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(425, 466)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txt_ip As TextBox
+    Friend WithEvents txt_mac As TextBox
+    Friend WithEvents txt_pc As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tm As Timer
+    Friend WithEvents tm2 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ LOG/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Net.NetworkInformation
+Imports System.Net
+Public Class Form1
+    Dim srv As New srv.Service1Client
+
+    <StructLayout(LayoutKind.Sequential)>
+    Structure LASTINPUTINFO
+        <MarshalAs(UnmanagedType.U4)>
+        Public cbSize As Integer
+        <MarshalAs(UnmanagedType.U4)>
+        Public dwTime As Integer
+    End Structure
+    <DllImport("user32.dll")>
+    Shared Function GetLastInputInfo(ByRef plii As LASTINPUTINFO) As Boolean
+    End Function
+
+    Dim idletime As Integer
+    Dim lastInputInf As New LASTINPUTINFO()
+
+    Public Function GetLastInputTime() As Integer
+        idletime = 0
+        lastInputInf.cbSize = Marshal.SizeOf(lastInputInf)
+        lastInputInf.dwTime = 0
+        If GetLastInputInfo(lastInputInf) Then
+            idletime = Environment.TickCount - lastInputInf.dwTime
+        End If
+        If idletime > 0 Then
+            Return idletime / 1000
+        Else : Return 0
+        End If
+    End Function
+
+    Private LastLastIdletime As Integer = 0
+    Public countMouseMove As Integer = 0
+    Dim ip As String
+    Dim machineId As String
+    Dim pcName As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tm.Enabled = True
+        tm.Start()
+        tm2.Enabled = True
+        tm2.Start()
+        Data()
+        Me.Hide()
+        Me.ShowInTaskbar = False
+        NotifyIcon1.ShowBalloonTip(500000, "Click", "Programi po punon ne background !", ToolTipIcon.Info)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        ShowInTaskbar = False
+    End Sub
+
+    Private Sub tm_Tick(sender As Object, e As EventArgs) Handles tm.Tick
+        Dim it As Integer = GetLastInputTime()
+        If LastLastIdletime > it Then
+            Label1.Text = "Statusi ndryshoi !"
+            countMouseMove += 1
+            'sumofidletime = sumofidletime.Add(TimeSpan.FromSeconds(LastLastIdletime))
+            'Label2.Text = "Totali i kohes inaktive : " & sumofidletime.ToString
+            Label3.Text = "Heret qe mouse ka levizur : " & countMouseMove.ToString
+        Else
+            Label1.Text = GetLastInputTime()
+        End If
+        LastLastIdletime = it
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Hide()
+        Me.ShowInTaskbar = False
+    End Sub
+
+    Public Sub Data()
+        Dim mac As String
+        Dim nics() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces
+        mac = nics(0).GetPhysicalAddress.ToString
+        txt_mac.Text = mac
+        machineId = mac
+        pcName = Dns.GetHostName
+        txt_pc.Text = pcName
+        Dim h As IPHostEntry = Dns.GetHostEntry(Dns.GetHostName)
+        For Each add As IPAddress In h.AddressList
+            If add.AddressFamily = Sockets.AddressFamily.InterNetwork Then
+                ip = h.AddressList(1).ToString
+                txt_ip.Text = ip
+            End If
+        Next
+    End Sub
+
+    Private Sub tm2_Tick(sender As Object, e As EventArgs) Handles tm2.Tick
+        Try
+            Dim c As Boolean
+            c = srv.insertPc(ip, machineId, countMouseMove)
+            countMouseMove = 0
+        Catch ex As Exception
+            countMouseMove = 0
+            Try
+                srv.insErr(ip, ex.Message)
+            Catch ex1 As Exception
+                MessageBox.Show("ERROR !")
+            End Try
+        End Try
+    End Sub
+End Class
